fafb052e9c621c446b3fb8d88b355caa0a69945b
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2024-03-18 18:25:35 +0400

    initial commit

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+declare module '*.scss';
+
+// declare module '*.scss' {
+//     interface IClassNames {
+//         [className: string]: string
+//     }
+//     const className: IClassNames;
+//     export = className;
+// }

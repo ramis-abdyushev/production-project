50033ddfd40eed2bf0e04acc74022d0874f10e3c
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2024-04-13 18:08:03 +0400

    Разработаны компоненты AppLink и Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import 'app/styles/index.scss';
-import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to='/'>Главная</Link>
-            <Link to='/about'>О сайте</Link>
-            <AppRouter></AppRouter>
         </div>
     );
 }
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,22 @@
+import cls from './Navbar.module.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to='/' theme={ AppLinkTheme.SECONDARY }>
+                    Главная
+                </AppLink>
+                <AppLink to='/about' theme={ AppLinkTheme.SECONDARY }>
+                    О сайте
+                </AppLink>
+            </div>
+        </div>
+    );
+};

ab6a1dad9bf278562b057ec6cad345ce6bee2588
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2024-08-12 18:49:50 +0300

    Фикс снимков

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -1,7 +1,7 @@
 import { Counter } from './Counter';
 import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { screen } from '@testing-library/react';
-import { userEvent } from '@storybook/testing-library';
+import userEvent from '@testing-library/user-event';
 
 describe('Counter', () => {
   test('with only first param', () => {

647972570d1cc6eee80f97bc6b9a9ad5d356d4fe
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2024-09-24 19:00:34 +0300

    Доработан роутер

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
 export { userReducer, userActions } from './model/slice/userSlice';
 export { User, UserSchema } from './model/types/user';
 export { getUserAuthData } from './model/selector/getUserAuthData/getUserAuthData';
+export { getUserInited } from './model/selector/getUserInited/getUserInited';

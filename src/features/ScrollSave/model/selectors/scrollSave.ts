b59658ff0ae00c7b513dd92a1d6a4a216ea903d1
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2024-10-15 18:43:21 +0300

    Fix getScrollSaveByPath

--- src/features/ScrollSave/model/selectors/scrollSave.ts
+++ src/features/ScrollSave/model/selectors/scrollSave.ts
@@ -1,12 +1,10 @@
 import { StateSchema } from 'app/providers/StoreProvider';
 import { createSelector } from '@reduxjs/toolkit';
 
-export const getScrollSave = (state: StateSchema) => state.scrollSave;
+export const getScrollSave = (state: StateSchema) => state.scrollSave.scroll;
 
 export const getScrollSaveByPath = createSelector(
   getScrollSave,
   (state: StateSchema, path: string) => path,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
   (scroll, path) => scroll[path] || 0,
 );
